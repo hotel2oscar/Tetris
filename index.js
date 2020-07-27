0b8105c414d9049e9f53232a99a5e82e3142de28
Waters <h2oboi89@gmail.com>
2020-07-26 20:51:48 -0400

    added state and FPS counter

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const canvas = document.getElementById('gameWindow');
 
 const ctx = canvas.getContext('2d');
 
-// ctx.strokeRect(10, 10, 10, 10);
-
-console.log(`H: ${canvas.height} x W: ${canvas.width}`);
-
 const BOARDWIDTH = 10;
 const BOARDHEIGHT = 20;
 const EDGEWIDTH = BOARDWIDTH + 1;
 const EDGEHEIGHT = BOARDHEIGHT + 1;
 const BLOCKSIZE = 20;
+
+const state = {
+    level : 0,
+    score : 0,
+    lines : 0,
+    board : [],
+    performance: {
+        fps : 0,
+        times : []
+    }
+};
 
 const drawBoard = () => {
     let startX = BLOCKSIZE;
@@ -42,7 +49,7 @@ const drawBoard = () => {
     drawEdge(EDGEHEIGHT, () => startY -= BLOCKSIZE);
 };
 
-const drawSideBar = (level, score, lines) => {
+const drawSideBar = (state) => {
     let startX = BLOCKSIZE * 15;
     let startY = BLOCKSIZE * 2;
 
@@ -54,19 +61,50 @@ const drawSideBar = (level, score, lines) => {
     ctx.font = `${BLOCKSIZE - 2}px Consolas`;
     
     ctx.fillText('LEVEL:', startX + 2 * BLOCKSIZE, startY + 9 * BLOCKSIZE);
-    ctx.fillText(`${level}`, startX + 3 * BLOCKSIZE, startY + 10 * BLOCKSIZE);
+    ctx.fillText(`${state.level}`, startX + 3 * BLOCKSIZE, startY + 10 * BLOCKSIZE);
 
     ctx.fillText('SCORE:', startX + 2 * BLOCKSIZE, startY + 12 * BLOCKSIZE);
-    ctx.fillText(`${score}`, startX + 3 * BLOCKSIZE, startY + 13 * BLOCKSIZE);
+    ctx.fillText(`${state.score}`, startX + 3 * BLOCKSIZE, startY + 13 * BLOCKSIZE);
 
     ctx.fillText('LINES:', startX + 2 * BLOCKSIZE, startY + 15 * BLOCKSIZE);
-    ctx.fillText(`${lines}`, startX + 3 * BLOCKSIZE, startY + 16 * BLOCKSIZE);
+    ctx.fillText(`${state.lines}`, startX + 3 * BLOCKSIZE, startY + 16 * BLOCKSIZE);
+
+    ctx.fillText('FPS:', startX + 2 * BLOCKSIZE, startY + 18 * BLOCKSIZE);
+    ctx.fillText(`${state.performance.fps}`, startX + 2 * BLOCKSIZE, startY + 19 * BLOCKSIZE);
 };
 
 const drawState = (state) => {
     // TODO: draw board state
 };
 
-drawBoard();
-drawSideBar(10, 1500, 50);
-drawState();
+const clear = () => {
+    ctx.clearRect(0, 0, canvas.height, canvas.width);
+}
+
+const tick = () => {
+    clear();
+    drawBoard();
+    drawSideBar(state);
+    drawState(state);
+};
+
+const updateFPS = () => {
+    const now = performance.now();
+    while(state.performance.times.length > 0 && state.performance.times[0] <= now - 1000) {
+        state.performance.times.shift();
+    }
+    state.performance.times.push(now);
+    state.performance.fps = state.performance.times.length;
+};
+
+const run = () => {
+    window.requestAnimationFrame(() => {
+        tick();
+        
+        updateFPS();
+
+        run();
+    });
+};
+
+run();
