d9bea503c42cba99d2abd91d30614ccb6368baea
Waters <h2oboi89@gmail.com>
2020-07-26 22:10:50 -0400

    implemented drawState

--- index.js
+++ index.js
@@ -2,86 +2,125 @@ const canvas = document.getElementById('gameWindow');
 
 const ctx = canvas.getContext('2d');
 
-const BOARDWIDTH = 10;
-const BOARDHEIGHT = 20;
-const EDGEWIDTH = BOARDWIDTH + 1;
-const EDGEHEIGHT = BOARDHEIGHT + 1;
-const BLOCKSIZE = 20;
+const CONSTANTS = {
+    BOARDWIDTH: 10,
+    BOARDHEIGHT: 20,
+    BLOCKSIZE: 20
+};
+
+const initializeBoard = () => {
+    let board = new Array(CONSTANTS.BOARDHEIGHT);
+
+    for (let i = 0; i < board.length; i++) {
+        board[i] = new Array(CONSTANTS.BOARDWIDTH);
+
+        for (let j = 0; j < board[i].length; j++) {
+            board[i][j] = null;
+        }
+    }
+
+    return board;
+}
 
 const state = {
-    level : 0,
-    score : 0,
-    lines : 0,
-    board : [],
+    level: 0,
+    score: 0,
+    lines: 0,
+    board: initializeBoard(),
     performance: {
-        fps : 0,
-        times : []
+        fps: 0,
+        times: []
     }
 };
 
+const drawBlock = (x, y, width) => {
+    ctx.fillRect(x, y, width, width);
+    ctx.strokeRect(x, y, width, width);
+};
+
 const drawBoard = () => {
-    let startX = BLOCKSIZE;
-    let startY = BLOCKSIZE;
-    
+    let startX = CONSTANTS.BLOCKSIZE;
+    let startY = CONSTANTS.BLOCKSIZE;
+    const EDGEWIDTH = CONSTANTS.BOARDWIDTH + 1;
+    const EDGEHEIGHT = CONSTANTS.BOARDHEIGHT + 1;
+
     ctx.fillStyle = 'rgb(82, 87, 94)';
     ctx.strokeStyle = 'rgb(53, 57, 61)';
 
     const drawEdge = (edgeSize, updateCoordinates) => {
         for (var i = 0; i < edgeSize; i++) {
 
-            ctx.fillRect(startX, startY, BLOCKSIZE, BLOCKSIZE);
-            ctx.strokeRect(startX, startY, BLOCKSIZE, BLOCKSIZE);
-    
+            drawBlock(startX, startY, CONSTANTS.BLOCKSIZE);
+
             updateCoordinates();
         }
     };
 
     // top
-    drawEdge(EDGEWIDTH, () => startX += BLOCKSIZE);
+    drawEdge(EDGEWIDTH, () => startX += CONSTANTS.BLOCKSIZE);
 
     // right
-    drawEdge(EDGEHEIGHT, () => startY += BLOCKSIZE);
+    drawEdge(EDGEHEIGHT, () => startY += CONSTANTS.BLOCKSIZE);
 
     // bottom
-    drawEdge(EDGEWIDTH, () => startX -= BLOCKSIZE);
+    drawEdge(EDGEWIDTH, () => startX -= CONSTANTS.BLOCKSIZE);
 
     // left
-    drawEdge(EDGEHEIGHT, () => startY -= BLOCKSIZE);
+    drawEdge(EDGEHEIGHT, () => startY -= CONSTANTS.BLOCKSIZE);
 };
 
 const drawSideBar = (state) => {
-    let startX = BLOCKSIZE * 15;
-    let startY = BLOCKSIZE * 2;
+    let startX = CONSTANTS.BLOCKSIZE * 15;
+    let startY = CONSTANTS.BLOCKSIZE * 2;
 
-    ctx.strokeRect(startX, startY, BLOCKSIZE * 8, BLOCKSIZE * 20);
-    
+    ctx.strokeRect(startX, startY, CONSTANTS.BLOCKSIZE * 8, CONSTANTS.BLOCKSIZE * 20);
+
     // next block window
-    ctx.strokeRect(startX + BLOCKSIZE, startY + BLOCKSIZE, BLOCKSIZE * 6, BLOCKSIZE * 6);
+    ctx.strokeRect(startX + CONSTANTS.BLOCKSIZE, startY + CONSTANTS.BLOCKSIZE, CONSTANTS.BLOCKSIZE * 6, CONSTANTS.BLOCKSIZE * 6);
 
-    ctx.font = `${BLOCKSIZE - 2}px Consolas`;
-    
-    ctx.fillText('LEVEL:', startX + 2 * BLOCKSIZE, startY + 9 * BLOCKSIZE);
-    ctx.fillText(`${state.level}`, startX + 3 * BLOCKSIZE, startY + 10 * BLOCKSIZE);
+    // TODO: draw next block
 
-    ctx.fillText('SCORE:', startX + 2 * BLOCKSIZE, startY + 12 * BLOCKSIZE);
-    ctx.fillText(`${state.score}`, startX + 3 * BLOCKSIZE, startY + 13 * BLOCKSIZE);
+    ctx.font = `${CONSTANTS.BLOCKSIZE - 2}px Consolas`;
 
-    ctx.fillText('LINES:', startX + 2 * BLOCKSIZE, startY + 15 * BLOCKSIZE);
-    ctx.fillText(`${state.lines}`, startX + 3 * BLOCKSIZE, startY + 16 * BLOCKSIZE);
+    const drawText = (label, value, y) => {
+        ctx.fillText(label + ':', startX + 2 * CONSTANTS.BLOCKSIZE, startY + y * CONSTANTS.BLOCKSIZE);
+        ctx.fillText(`${value}`, startX + 3 * CONSTANTS.BLOCKSIZE, startY + (y + 1) * CONSTANTS.BLOCKSIZE);
+    };
 
-    ctx.fillText('FPS:', startX + 2 * BLOCKSIZE, startY + 18 * BLOCKSIZE);
-    ctx.fillText(`${state.performance.fps}`, startX + 2 * BLOCKSIZE, startY + 19 * BLOCKSIZE);
+    drawText('LEVEL', state.level, 9);
+    drawText('SCORE', state.score, 12);
+    drawText('LINES', state.lines, 15);
+    drawText('FPS', state.performance.fps, 18);
 };
 
 const drawState = (state) => {
-    // TODO: draw board state
+    ctx.strokeStyle = 'rgb(0, 0, 0)';
+
+    for (let y = 0; y < state.board.length; y++) {
+        for (let x = 0; x < state.board[y].length; x++) {
+            if (state.board[y][x] !== null) {
+
+                ctx.fillStyle = state.board[y][x];
+
+                // canvas 0,0 is top left, we need 0,0 to be bottom left and offset from edge like board
+                let actualX = (2 + x) * CONSTANTS.BLOCKSIZE;
+                let actualY = (CONSTANTS.BOARDHEIGHT - y + 1) * CONSTANTS.BLOCKSIZE;
+
+                drawBlock(actualX, actualY, CONSTANTS.BLOCKSIZE);
+            }
+        }
+    }
 };
 
 const clear = () => {
     ctx.clearRect(0, 0, canvas.height, canvas.width);
 }
 
-const tick = () => {
+const updateState = () => {
+    // TODO: update game state
+};
+
+const updateUI = () => {
     clear();
     drawBoard();
     drawSideBar(state);
@@ -90,7 +129,7 @@ const tick = () => {
 
 const updateFPS = () => {
     const now = performance.now();
-    while(state.performance.times.length > 0 && state.performance.times[0] <= now - 1000) {
+    while (state.performance.times.length > 0 && state.performance.times[0] <= now - 1000) {
         state.performance.times.shift();
     }
     state.performance.times.push(now);
@@ -99,12 +138,22 @@ const updateFPS = () => {
 
 const run = () => {
     window.requestAnimationFrame(() => {
-        tick();
-        
+        updateState();
+
+        updateUI();
+
         updateFPS();
 
         run();
     });
 };
+
+console.log(state.board);
+state.board[0][0] = 'rgb(0, 0, 255)';
+state.board[0][9] = 'rgb(0, 0, 255)';
+state.board[10][0] = 'rgb(0, 255, 0)';
+state.board[10][9] = 'rgb(0, 255, 0)';
+state.board[19][0] = 'rgb(255, 0, 0)';
+state.board[19][9] = 'rgb(255, 0, 0)';
 
 run();
--- notes.md
+++ notes.md
@@ -3,6 +3,9 @@
 ## Layout
 10 wide x 20 tall grid + room for side window (next piece, score, rows, etc)
 
+## Board
+each space in block will have either `null` or a color in it
+
 ## Misc
 Figure out keys from controller to allow keyboard / controller controls
 
