e4afe21d9ff57857bc66e883263c57d8460ad876
Waters <h2oboi89@gmail.com>
2020-07-27 00:28:16 -0400

    user input from keyboard and controller

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const initializeBoard = () => {
     }
 
     return board;
-}
+};
 
 const state = {
     level: 0,
@@ -30,7 +30,8 @@ const state = {
     performance: {
         fps: 0,
         times: []
-    }
+    },
+    stop: undefined
 };
 
 const drawBlock = (x, y, width) => {
@@ -112,19 +113,139 @@ const drawState = (state) => {
     }
 };
 
+class Key {
+    static _pressed = {};
+
+    static SPACE = 32;
+    static A = 65;
+    static B = 66;
+    static LEFT = 37;
+    static UP = 38;
+    static RIGHT = 39;
+    static DOWN = 40;
+
+    static isDown(keyCode) {
+        return Key._pressed[keyCode];
+    }
+
+    static onKeyDown(event) {
+        Key._pressed[event.keyCode] = true;
+    }
+
+    static onKeyUp(event) {
+        Key._pressed[event.keyCode] = false;
+    }
+};
+
+class Gamepad {
+    static A = 0;
+    static B = 1;
+    static START = 9;
+    static UP = 12;
+    static DOWN = 13;
+    static LEFT = 14;
+    static RIGHT = 15
+
+    static isDown(buttonCode) {
+        var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+
+        if(gamepads === undefined) return false;
+        
+        let gamepad = gamepads[0];
+
+        if (gamepad === undefined || gamepad === null) return false;
+
+        return gamepad.buttons[buttonCode].pressed;
+    }
+}
+
+window.addEventListener('keydown', (event) => Key.onKeyDown(event), false);
+window.addEventListener('keyup', (event) => Key.onKeyUp(event), false);
+
+window.addEventListener("gamepadconnected", function (e) {
+    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+        e.gamepad.index, e.gamepad.id,
+        e.gamepad.buttons.length, e.gamepad.axes.length);
+});
+
 const clear = () => {
     ctx.clearRect(0, 0, canvas.height, canvas.width);
-}
+};
+
+let inputEnabled = true;
+
+const determineUserInput = () => {
+    // TODO: use setTimeout to disable userinput for X ms?
+    // TODO: determine if needed when testing rotation (A & B)
+    // if (inputEnabled == false) {
+    //     return;
+    // }
+
+    // inputEnabled = false;
+
+    // setTimeout(() => inputEnabled = true, 100);
+
+    if (Key.isDown(Key.A)) {
+        console.log("Key A is pressed");
+    }
+
+    if (Key.isDown(Key.B)) {
+        console.log("Key B is pressed");
+    }
+
+    if (Key.isDown(Key.LEFT)) {
+        console.log("Key LEFT is pressed");
+    }
+
+    if (Key.isDown(Key.RIGHT)) {
+        console.log("Key RIGHT is pressed");
+    }
+
+    if (Key.isDown(Key.UP)) {
+        console.log("Key UP is pressed");
+    }
+
+    if (Key.isDown(Key.DOWN)) {
+        console.log("Key DOWN is pressed");
+    }
+
+    if (Key.isDown(Key.SPACE)) {
+        console.log("Key SPACE is pressed");
+    }
+
+    if (Gamepad.isDown(Gamepad.A)) {
+        console.log("Gamepad A is pressed");
+    }
+
+    if (Gamepad.isDown(Gamepad.B)) {
+        console.log("Gamepad B is pressed");
+    }
+
+    if (Gamepad.isDown(Gamepad.LEFT)) {
+        console.log("Gamepad LEFT is pressed");
+    }
+
+    if (Gamepad.isDown(Gamepad.RIGHT)) {
+        console.log("Gamepad RIGHT is pressed");
+    }
+
+    if (Gamepad.isDown(Gamepad.UP)) {
+        console.log("Gamepad UP is pressed");
+    }
+
+    if (Gamepad.isDown(Gamepad.DOWN)) {
+        console.log("Gamepad DOWN is pressed");
+    }
+
+    if (Gamepad.isDown(Gamepad.START)) {
+        console.log("Gamepad START is pressed");
+    }
+
+    // TODO: cancel out opposite inputs
+};
 
 const updateState = () => {
     // TODO: update game state
-};
-
-const updateUI = () => {
-    clear();
-    drawBoard();
-    drawSideBar(state);
-    drawState(state);
 };
 
 const updateFPS = () => {
@@ -136,19 +257,6 @@ const updateFPS = () => {
     state.performance.fps = state.performance.times.length;
 };
 
-const run = () => {
-    window.requestAnimationFrame(() => {
-        updateState();
-
-        updateUI();
-
-        updateFPS();
-
-        run();
-    });
-};
-
-console.log(state.board);
 state.board[0][0] = 'rgb(0, 0, 255)';
 state.board[0][9] = 'rgb(0, 0, 255)';
 state.board[10][0] = 'rgb(0, 255, 0)';
@@ -156,4 +264,25 @@ state.board[10][9] = 'rgb(0, 255, 0)';
 state.board[19][0] = 'rgb(255, 0, 0)';
 state.board[19][9] = 'rgb(255, 0, 0)';
 
-run();
+const update = (frameTimestamp) => {
+    determineUserInput();
+
+    updateState();
+
+    updateFPS();
+};
+
+const render = () => {
+    clear();
+    drawBoard();
+    drawSideBar(state);
+    drawState(state);
+};
+
+(function run(frameTimestamp) {
+    state.stop = window.requestAnimationFrame(run);
+
+    update(frameTimestamp);
+
+    render();
+})();
